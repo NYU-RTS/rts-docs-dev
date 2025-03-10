d48993a151faa8e2b15f86100b5fe2044a8b1c54
Robert Young <robert.jay.young@gmail.com>
2025-03-10 16:52:08 -0400

    added app.py for vast as per convo with Carl; untested; will prob need tweaking

---
+++ vast/app.py
@@ -0,0 +1,56 @@
+import logging
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+from typing import (
+    List, Optional
+)
+import urllib3
+import gpfs as gpfs
+
+class Group(BaseModel):
+    name: str
+    description: str = None
+
+class GroupMembers(BaseModel):
+    name: str
+    users: Optional[list] = None
+
+app = FastAPI()
+gpfs = gpfs.GPFS()
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+logging = logging.getLogger("uvicorn")
+
+@app.get('/')
+def index():
+    return {'data': 'Go away please'}
+
+@app.get('/healthz')
+def index():
+    return {'status': 'ok'}
+
+@app.get('/message')
+def index():
+    return {'data': 'FastAPI is easy!'}
+
+@app.get('/vast_user_quota/{user_id}')
+def vast_get_user_quota(user_id):
+    try:
+        quota = vast.get_user_quota(user_id)
+    except Exception as err:
+       logging.error(f'Error reading user quota {user_id}: {err}')
+    return {'data': quota }
+
+@app.get('/vast_user_quotas')
+def vast_get_all_user_quotas():
+    try:
+        quotas = vast.get_user_quotas()
+    except Exception as err:
+       logging.error(f'Error reading all vast user quotas: {err}')
+    return {'data': quotas }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8080)
